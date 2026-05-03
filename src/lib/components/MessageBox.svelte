b0ae5ca8a203d7567e1f1c4848d6a5e810ad1eff
Dimitar <19364673+Dimitar5555@users.noreply.github.com>
2026-05-03 22:47:39 +0300

    use bootstrap classes for messagebox

--- src/lib/components/MessageBox.svelte
+++ src/lib/components/MessageBox.svelte
@@ -1,9 +1,9 @@
 <script lang="ts">
     let { successMessage, errorMessage }: { successMessage: string, errorMessage: string } = $props();
 </script>
-<div style="background-color: lightgreen; padding: 10px; margin-bottom: 10px;" hidden={!successMessage}>
+<div class="alert alert-success" role="alert" hidden={!successMessage}>
     {successMessage}
 </div>
-<div style="background-color: lightcoral; padding: 10px; margin-bottom: 10px;" hidden={!errorMessage}>
+<div class="alert alert-danger" role="alert" hidden={!errorMessage}>
     {errorMessage}
 </div>
