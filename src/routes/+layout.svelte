5c44e30fe759bdf96c2f26ae498ce2ab6adf65df
Dimitar <19364673+Dimitar5555@users.noreply.github.com>
2026-05-03 22:47:00 +0300

    add boostrap and bootstrap icons

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,16 @@
 <script lang="ts">
 	// import favicon from '$lib/assets/favicon.svg';
+ 	import 'bootstrap/dist/css/bootstrap.css';
+    import scriptSrc from 'bootstrap/dist/js/bootstrap.bundle.js?url';
+	import 'bootstrap-icons/font/bootstrap-icons.css';
+
 
 	let { children } = $props();
 </script>
 
 <svelte:head>
 	<!-- <link rel="icon" href={favicon} /> -->
+	<script src={scriptSrc}></script>
 </svelte:head>
 
 {@render children()}
