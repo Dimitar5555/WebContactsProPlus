90dc413a5130d4a3ccd44a2e73872697d691ab02
Dimitar <19364673+Dimitar5555@users.noreply.github.com>
2026-06-04 15:32:42 +0300

    Update ContactForm.svelte

--- src/lib/components/ContactForm.svelte
+++ src/lib/components/ContactForm.svelte
@@ -3,6 +3,7 @@
     let { data = $bindable(), photo_file = $bindable(), remove_photo = $bindable(), saveBtnLabel }: any = $props();
     let contact = $state(data.contact);
     let phoneNumbers = $state(data.phone_numbers);
+    let canvas = $state<HTMLCanvasElement>();
 </script>
 
 <input type="hidden" name="contact_id" bind:value={contact.id} />
@@ -10,22 +11,24 @@
     <div class="col-12 col-md-3">
         <div class="d-flex flex-column align-items-center">
             <label>Снимка</label>
-            <canvas width="90" height="90" class="border"></canvas>
+            {#if contact.photo_url}
+                <img src={`/api/v1/photos/thumb_${contact.photo_url}`} alt="Contact Photo" width="90" height="90" class="border mb-2" />
+            {/if}
+            <canvas width="90" height="90" class="border" bind:this={canvas}></canvas>
             <input type="file" name="contactPhoto" accept="image/*" onchange={async (e) => {
-                const canvasEl = document.querySelector('canvas') as HTMLCanvasElement;
                 const fileReader = new FileReader();
                 photo_file = (e.target as HTMLInputElement).files[0] as File;
                 if(photo_file) {
                     fileReader.readAsDataURL(photo_file);
                     fileReader.onload = (event) => {
-                        canvasEl?.getContext('2d')?.clearRect(0, 0, canvasEl.width, canvasEl.height);
+                        canvas.getContext('2d')?.clearRect(0, 0, canvas.width, canvas.height);
                         const img = new Image();
                         img.onload = () => {
-                            const ctx = canvasEl?.getContext('2d');
+                            const ctx = canvas.getContext('2d');
                             if(ctx) {
-                                const scale = Math.min(canvasEl.width / img.width, canvasEl.height / img.height);
-                                const x = (canvasEl.width / 2) - (img.width / 2) * scale;
-                                const y = (canvasEl.height / 2) - (img.height / 2) * scale;
+                                const scale = Math.min(canvas.width / img.width, canvas.height / img.height);
+                                const x = (canvas.width / 2) - (img.width / 2) * scale;
+                                const y = (canvas.height / 2) - (img.height / 2) * scale;
                                 ctx.drawImage(img, x, y, img.width * scale, img.height * scale);
                             }
                         };
@@ -38,8 +41,7 @@
             <br/>
             <button type="button" onclick={() => {
                 contact.photo_url = null;
-                const canvasEl = document.querySelector('canvas') as HTMLCanvasElement;
-                canvasEl?.getContext('2d')?.clearRect(0, 0, canvasEl.width, canvasEl.height);
+                canvas.getContext('2d')?.clearRect(0, 0, canvas.width, canvas.height);
                 const photoInput = document.querySelector('input[type="file"]') as HTMLInputElement;
                 if(photoInput) {
                     photoInput.value = '';
